0ec2ed1e319c6db78814a0be1072709694cb9d1b
Saloni Saboo <114433313+salonisaboo@users.noreply.github.com>
2026-02-19 11:32:04 +0530

    Update UserController.java

--- backend/InventoryMgtSys/src/main/java/com/riya/InventoryMgtSys/controllers/UserController.java
+++ backend/InventoryMgtSys/src/main/java/com/riya/InventoryMgtSys/controllers/UserController.java
@@ -1,4 +1,4 @@
-package com.riya.InventoryMgtSys.controllers;
+package com.project.InventoryMgtSys.controllers;
 
 import com.riya.InventoryMgtSys.dtos.Response;
 import com.riya.InventoryMgtSys.dtos.UserDTO;
