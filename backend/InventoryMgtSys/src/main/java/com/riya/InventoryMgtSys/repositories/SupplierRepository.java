9294086a60c7a04a980621990b64a4b324d1aba3
Saloni Saboo <114433313+salonisaboo@users.noreply.github.com>
2026-02-19 13:10:38 +0530

    Update SupplierRepository.java

--- backend/InventoryMgtSys/src/main/java/com/riya/InventoryMgtSys/repositories/SupplierRepository.java
+++ backend/InventoryMgtSys/src/main/java/com/riya/InventoryMgtSys/repositories/SupplierRepository.java
@@ -1,6 +1,6 @@
-package com.riya.InventoryMgtSys.repositories;
+package com.project.InventoryMgtSys.repositories;
 
-import com.riya.InventoryMgtSys.models.Supplier;
+import com.project.InventoryMgtSys.models.Supplier;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface SupplierRepository extends JpaRepository<Supplier, Long> {
