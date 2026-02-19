b0b558189691ef6072e8e2197371f180547f5077
Saloni Saboo <114433313+salonisaboo@users.noreply.github.com>
2026-02-19 13:13:24 +0530

    Update CorsConfig.java

--- backend/InventoryMgtSys/src/main/java/com/riya/InventoryMgtSys/security/CorsConfig.java
+++ backend/InventoryMgtSys/src/main/java/com/riya/InventoryMgtSys/security/CorsConfig.java
@@ -1,4 +1,4 @@
-package com.riya.InventoryMgtSys.security;
+package com.project.InventoryMgtSys.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
