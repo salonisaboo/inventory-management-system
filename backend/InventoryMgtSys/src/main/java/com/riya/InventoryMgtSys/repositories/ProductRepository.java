3a154cdb39db1ef8279fc86e4805f02880a42e5c
Saloni Saboo <114433313+salonisaboo@users.noreply.github.com>
2026-02-19 13:10:09 +0530

    Update ProductRepository.java

--- backend/InventoryMgtSys/src/main/java/com/riya/InventoryMgtSys/repositories/ProductRepository.java
+++ backend/InventoryMgtSys/src/main/java/com/riya/InventoryMgtSys/repositories/ProductRepository.java
@@ -1,6 +1,6 @@
-package com.riya.InventoryMgtSys.repositories;
+package com.project.InventoryMgtSys.repositories;
 
-import com.riya.InventoryMgtSys.models.Product;
+import com.project.InventoryMgtSys.models.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
